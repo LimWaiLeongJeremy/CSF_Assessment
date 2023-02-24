b249f49aa92daddc7afb165e625b8c136bf29d4b
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-02-24 13:21:10 +0800

    2

--- client/src/app/components/cuisine-list.component.ts
+++ client/src/app/components/cuisine-list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Restaurant } from '../models';
+import { RestaurantService } from '../restaurant-service';
 
 @Component({
   selector: 'app-cuisine-list',
   templateUrl: './cuisine-list.component.html',
   styleUrls: ['./cuisine-list.component.css']
 })
-export class CuisineListComponent {
+export class CuisineListComponent implements OnInit{
 
-	// TODO Task 2
-	// For View 1
+  restaurants: Restaurant[] = []
+  constructor(private svc: RestaurantService) { }
+
+  ngOnInit(): void {
+      this.getRestaurants()
+  }
+
+  getRestaurants() {
+    console.log('Getting restaurants!');
+    this.svc.getRestaurants()
+                      .subscribe(data => {
+                          this.restaurants = data;
+                          console.log(data)
+  
+    })
+  }
+
 
 }
--- client/src/app/restaurant-service.ts
+++ client/src/app/restaurant-service.ts
@@ -1,39 +1,48 @@
 import { Restaurant, Comment } from './models'
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
+@Injectable()
 export class RestaurantService {
 
+	constructor(private http: HttpClient) { }
 	// TODO Task 2 
 	// Use the following method to get a list of cuisines
 	// You can add any parameters (if any) and the return type 
 	// DO NOT CHNAGE THE METHOD'S NAME
-	public getCuisineList(???) {
-		// Implememntation in here
+	// public getCuisineList(???) {
+	// 	// Implememntation in here
 
-	}
+	// }
+
+    getRestaurants(): Observable<Restaurant[]> {
+        return this.http.get<Restaurant[]>(`/api/restaurants`)
+    }
 
 	// TODO Task 3 
 	// Use the following method to get a list of restaurants by cuisine
 	// You can add any parameters (if any) and the return type 
 	// DO NOT CHNAGE THE METHOD'S NAME
-	public getRestaurantsByCuisine(???) {
-		// Implememntation in here
+	// public getRestaurantsByCuisine(???) {
+	// 	// Implememntation in here
 
-	}
+	// }
 	
 	// TODO Task 4
 	// Use this method to find a specific restaurant
 	// You can add any parameters (if any) 
 	// DO NOT CHNAGE THE METHOD'S NAME OR THE RETURN TYPE
-	public getRestaurant(???): Promise<Restaurant> {
-		// Implememntation in here
+	// public getRestaurant(???): Promise<Restaurant> {
+	// 	// Implememntation in here
 
-	}
+	// }
 
 	// TODO Task 5
 	// Use this method to submit a comment
 	// DO NOT CHANGE THE METHOD'S NAME OR SIGNATURE
-	public postComment(comment: Comment): Promise<any> {
-		// Implememntation in here
+	// public postComment(comment: Comment): Promise<any> {
+	// 	// Implememntation in here
 
-	}
+	// }
 }
